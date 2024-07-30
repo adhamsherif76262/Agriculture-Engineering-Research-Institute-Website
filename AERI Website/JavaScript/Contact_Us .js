1cf45ca7f7e59093a941630290a3400b7921f645
Adham Sherif <adhamsherif7261@gmail.com>
2024-07-31 02:40:25 +0300

    Language Saving Across Paegs Problem Solved :)

---
+++ AERI Website/JavaScript/Contact_Us .js
@@ -0,0 +1,14 @@
+setTimeout(()=>{
+    console.log(Language)
+},1000)
+
+if(Language = "عربي"){
+    console.log(" arabbbb")
+    const h1 = document.getElementById("7amada");
+    h1.textContent = "اسنلايشسهياشتسياتشغسلعايىشتستيغلهشعاس"
+}
+else if(Language = "English"){
+    console.log(" Engggggg")
+    const h1 = document.getElementById("7amada");
+    h1.textContent = "Agricultural Engineering Research Institute Website"
+}
--- AERI Website/JavaScript/NavBar_Footer_Language Changer.js
+++ AERI Website/JavaScript/NavBar_Footer_Language Changer.js
@@ -6,7 +6,8 @@ var Upper_NavBar_Ar = document.getElementById("Upper_NavBar_Ar");
 var PageTitle = document.getElementsByTagName("title");
 var title = PageTitle[0].innerHTML;
 var navItems = document.querySelectorAll(".Nav_Links");
-
+var Language= "";
+console.log("Hel")
 var Ar_NavTemplate = `
           <div id="Upper_NavBar_Ar" class="Flex_Direction_Ar">
               <img class="NavBar_Logos" width="100" height="100" src="../Images/AERI Logo .jpg" alt="Agriculture Engineering Institute Logo">
@@ -30,7 +31,8 @@ var Ar_NavTemplate = `
           </div>
     `;
 
-var En_NavTemplate = `<div id="Upper_NavBar_En" class="Flex_Direction_En">
+var En_NavTemplate = `
+        <div id="Upper_NavBar_En" class="Flex_Direction_En">
             <img class="NavBar_Logos" width="100" height="100" src="../Images/AERI_Logo_-removebg-preview.png" alt="Agriculture Engineering Institute Logo">
             <p id="NavBar_P">Agriculture Engineering Research Institute</p>
             <img class="NavBar_Logos" width="150" height="100" src="../Images/ARC_Logo_-removebg-preview.png" alt="Agriculture Engineering Institute Logo">
@@ -49,7 +51,8 @@ var En_NavTemplate = `<div id="Upper_NavBar_En" class="Flex_Direction_En">
                 <a onmouseover="Mouse_Over(this);" onmouseout="Mouse_Leave(this);" class="Nav_Links Nav_Links_En" href="../HTML/Home.html">Home</a>
             </div>
           </div>
-        </div>`;
+        </div>
+    `;
 
 title = title.slice(5);
 title = title.substring(0, title.length - 5);
@@ -91,50 +94,55 @@ function Mouse_Leave(anchor){
   .css({ 
     transition:"0.5s",
     opacity:"1",
-            filter:"blur(0px)",
-          });
-          $(".Nav_Links_En").css({
-            width: "min-content",
-            height: "fit-content",
-            fontSize: "25px",
-            color: "Black",
-          });
-          $(".Nav_Links_Ar").css({
-            width: "fit-content",
-            height: "fit-content",
-            fontSize: "25px",
-            color: "Black",
-          });
+    filter:"blur(0px)",
+  });
+  $(".Nav_Links_En").css({
+    width: "min-content",
+    height: "fit-content",
+    fontSize: "25px",
+    color: "Black",
+  });
+  $(".Nav_Links_Ar").css({
+    width: "fit-content",
+    height: "fit-content",
+    fontSize: "25px",
+    color: "Black",
+  });
 }
 const xhr = new XMLHttpRequest();
 xhr.open(
-    "GET",
-    "https://api.geoapify.com/v1/ipinfo?&apiKey=b1eafd427c864fd8ad4009a48be4ec1f"
-  );
-  xhr.onload = function(){
-    JSON.parse(xhr.responseText);
-    console.log(JSON.parse(xhr.responseText));
-    if (JSON.parse(xhr.responseText).country.languages[0].name_native === "العربية") {
-      Ar_Nav.innerHTML = Ar_NavTemplate;
-      LangBtn.innerHTML = "English";
-      En_Nav.classList.add("D_None");
-      Ar_Nav.classList.remove("D_None");
-    } else {
-      En_Nav.innerHTML = En_NavTemplate;
-      LangBtn.innerHTML = "عربي";
-      Ar_Nav.classList.add("D_None");
+  "GET",
+  "https://api.geoapify.com/v1/ipinfo?&apiKey=b1eafd427c864fd8ad4009a48be4ec1f"
+);
+xhr.onload = function(){
+  JSON.parse(xhr.responseText);
+  console.log(JSON.parse(xhr.responseText));
+  if (JSON.parse(xhr.responseText).country.languages[0].name_native === "العربية") {
+    Ar_Nav.innerHTML = Ar_NavTemplate;
+    LangBtn.innerHTML = "English";
+    En_Nav.classList.add("D_None");
+    Ar_Nav.classList.remove("D_None");
+    Language = "عربي";
+    console.log(Language)
+  } else {
+    En_Nav.innerHTML = En_NavTemplate;
+    LangBtn.innerHTML = "عربي";
+    Ar_Nav.classList.add("D_None");
       En_Nav.classList.remove("D_None");
+      Language = "English";
+      console.log(Language)
     }
   }
     
   xhr.send();
 
-document.addEventListener("DOMContentLoaded", function () {
-
-  if (LangBtn.innerHTML === "ُEnglish") {
-    En_Nav.classList.add("D_None");
-    Ar_Nav.classList.remove("D_None");
-  } else if (LangBtn.innerHTML === "عربي") {
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    
+    if (LangBtn.innerHTML === "ُEnglish") {
+      En_Nav.classList.add("D_None");
+      Ar_Nav.classList.remove("D_None");
+    } else if (LangBtn.innerHTML === "عربي") {
     Ar_Nav.classList.add("D_None");
     En_Nav.classList.remove("D_None");
   }
@@ -143,24 +151,6 @@ document.addEventListener("DOMContentLoaded", function () {
 En_Nav.classList.add("Text_Direction_En");
 Ar_Nav.classList.add("Text_Direction_Ar");
 
-LangBtn.addEventListener("click", function () {
-  if (LangBtn.innerHTML === "English") {
-    console.log("666666666666666666");
-    PageTitle[0].innerHTML = "الرئيسية";
-    PageTitle[0].setAttribute("lang", "ar");
-    LangBtn.innerHTML = "عربي";
-    Ar_Nav.classList.add("D_None");
-    En_Nav.classList.remove("D_None");
-    En_Nav.innerHTML = En_NavTemplate;
-  } else if (LangBtn.innerHTML === "عربي") {
-    PageTitle[0].innerHTML = "AERI Home Page";
-    PageTitle[0].setAttribute("lang", "en");
-    LangBtn.innerHTML = "English";
-    Ar_Nav.innerHTML = Ar_NavTemplate;
-    En_Nav.classList.add("D_None");
-    Ar_Nav.classList.remove("D_None");
-  }
-});
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
 window.onscroll = function () {
   scrollFunction();
@@ -175,3 +165,5 @@ function scrollFunction() {
     document.getElementById("English_NavBar").style.padding = "30px 10px";
   }
 }
+
+exports = {Language};
